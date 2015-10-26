f02dd7e3e6ed95c98b7a406406caee1eb103c1ab
cash <cash@cashwu.com>
2015-10-27 01:47:11 +0800

    Init

---
+++ DapperClassHelper/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DapperClassHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSqlServer = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbDbs = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sql Server";
+            // 
+            // tbSqlServer
+            // 
+            this.tbSqlServer.Location = new System.Drawing.Point(72, 10);
+            this.tbSqlServer.Name = "tbSqlServer";
+            this.tbSqlServer.Size = new System.Drawing.Size(136, 22);
+            this.tbSqlServer.TabIndex = 1;
+            this.tbSqlServer.Text = ".\\SQLEXPRESS";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(214, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connecting";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cbDbs
+            // 
+            this.cbDbs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDbs.FormattingEnabled = true;
+            this.cbDbs.Location = new System.Drawing.Point(15, 38);
+            this.cbDbs.Name = "cbDbs";
+            this.cbDbs.Size = new System.Drawing.Size(121, 20);
+            this.cbDbs.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 371);
+            this.Controls.Add(this.cbDbs);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbSqlServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSqlServer;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbDbs;
+    }
+}
+
---
+++ DapperClassHelper/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DapperClassHelper
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _connectionString = "Server={0};Integrated Security=SSPI";
+        private string _sqlServer;
+        private SqlConnection _connection;
+
+        private readonly string _getSqlServerDb = "SELECT name AS DbName FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.tbSqlServer.Text))
+            {
+                MessageBox.Show("Not Sql Server");  
+                return;
+            }
+
+            cbDbs.Items.Clear();
+
+            _sqlServer = tbSqlServer.Text;
+
+            this.GetDatabaseNames();
+        }
+
+        private void GetDatabaseNames()
+        {
+            this.GetConnection();
+
+            try
+            {
+                using (var conn = this._connection)
+                {
+                    var dbs =
+                        conn.Query<Db>(_getSqlServerDb).ToList();
+
+                    if (dbs.Count > 0)
+                    {
+                        cbDbs.DisplayMember = "DbName";
+                        cbDbs.ValueMember = "DbName";
+
+                        foreach (var db in dbs)
+                        {
+                            cbDbs.Items.Add(db);
+                        }
+
+                        cbDbs.SelectedIndex = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ..." + ex.ToString());
+            }
+        }
+
+        private SqlCommand GetCommand(string sql)
+        {
+            return this.GetCommand(sql, CommandType.Text);
+        }
+
+        private SqlCommand GetCommand(string sql, CommandType comType)
+        {
+            SqlCommand sqlCommand = new SqlCommand()
+            {
+                CommandText = sql,
+                CommandType = comType
+            };
+
+            sqlCommand.Connection = this._connection;
+
+            return sqlCommand;
+        }
+
+        private void GetConnection()
+        {
+            this._connection = new SqlConnection(string.Format(this._connectionString, _sqlServer));
+        }
+    }
+
+    internal class Db
+    {
+        public string DbName { get; set; }  
+    }
+}
